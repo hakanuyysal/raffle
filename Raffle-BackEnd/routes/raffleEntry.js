67e9bf0a5bf0570ce5169cc5f0cc43663230a98c
ItsBilge <bilgenursaglam@gmail.com>
2023-12-22 18:17:01 +0300

    updatebackend

--- Raffle-BackEnd/routes/raffleEntry.js
+++ Raffle-BackEnd/routes/raffleEntry.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const router = require("express").Router();
 const RaffleEntry = require("../models/RaffleEntry");
 const Match = require("../models/Matches");
@@ -7,26 +8,61 @@ router.post("/add-user", async (req, res) => {
   const { name, team } = req.body;
 
   try {
-    const newUser = new RaffleEntry({
-      name,
-      team,
-    });
-    const savedUser = await newUser.save();
-    const existingTeam = await Team.findOne({ name: team });
+    // Aynı isimde kullanıcıyı kontrol et
+    const existingUser = await RaffleEntry.findOne({ name, team });
 
-    if (existingTeam) {
-      existingTeam.members.push(newUser._id);
-      await existingTeam.save();
-    } else {
-      const newTeam = new Team({
-        name: team,
-        members: [newUser._id],
+    if (existingUser) {
+      return res.status(400).json({
+        success: false,
+        message: "Bu isimde bir katılımcı zaten var.",
       });
-      await newTeam.save();
     }
-    res.status(200).json(savedUser);
+    if (!team) {
+      return res
+        .status(400)
+        .json({ success: false, message: "Takım adı zorunludur" });
+    }
+
+    let existingTeam = await Team.findOne({ name: team });
+
+    if (!existingTeam) {
+      // Eğer takım yoksa, yeni bir takım oluştur
+      existingTeam = new Team({ name: team, members: [] });
+      await existingTeam.save();
+    }
+
+    if (name) {
+      // Eğer isim de varsa, kullanıcıyı kaydet
+      const newUser = new RaffleEntry({ name, team });
+      const savedUser = await newUser.save();
+
+      existingTeam.members.push(savedUser._id);
+      await existingTeam.save();
+
+      return res.status(200).json(savedUser);
+    }
+
+    res
+      .status(400)
+      .json({ success: false, message: "Katılımcı adı zorunludur" });
   } catch (error) {
     res.status(500).json(error);
+  }
+});
+
+router.delete("/delete-user/:name", async (req, res) => {
+  const { name } = req.params;
+  try {
+    const deletedParticipant = await RaffleEntry.findOneAndDelete({ name });
+
+    if (!deletedParticipant) {
+      return res
+        .status(404)
+        .json({ success: false, message: "Katılımcı bulunamadı." });
+    }
+    res.status(200).json("Katılımcı silinci");
+  } catch (error) {
+    res.status(500).json("Katılımcı silinirken bir hata oluştu.");
   }
 });
 
@@ -48,13 +84,19 @@ router.get("/match-users/:teamName", async (req, res) => {
   try {
     const teamName = req.params.teamName;
 
+    // console.log(teamName);
+
     if (!teamName) {
       return res
         .status(400)
         .json({ success: false, message: "Ekip adı belirtilmedi" });
     }
 
-    const team = await Team.findOne({ name: teamName }).populate("members");
+    const team = await Team.findOne({
+      name: { $regex: new RegExp(`^${teamName}$`, "i") },
+    });
+
+    // console.log(team);
 
     if (!team) {
       return res
@@ -62,8 +104,31 @@ router.get("/match-users/:teamName", async (req, res) => {
         .json({ success: false, message: "Belirtilen ekip bulunamadı" });
     }
 
-    const matches = matchTeamMembers(team.members);
-    res.json({ success: true, matches });
+    const teamMembers = await RaffleEntry.find({ team: team._id });
+    const matches = matchTeamMembers(teamMembers);
+
+    // Eğer eşleşme varsa, ilk eşleşmenin detaylarını al
+    let matchedUserDetails = null;
+    if (matches.length > 0) {
+      const firstMatch = matches[0];
+      const matchedUserId =
+        firstMatch.user2._id.toString() === team.members[0]._id.toString()
+          ? firstMatch.user1._id
+          : firstMatch.user2._id;
+
+      const matchedUser = await RaffleEntry.findById(matchedUserId);
+      matchedUserDetails = {
+        _id: matchedUser._id,
+        name: matchedUser.name,
+        team: teamName,
+      };
+    }
+
+    res.json({
+      success: true,
+      message: "Ekip üyeleri arasında eşleşme yapıldı",
+      matchedUserDetails,
+    });
   } catch (error) {
     res.status(500).json({
       success: false,
@@ -177,130 +242,6 @@ function matchTeamMembers(members) {
   }
 
   return matches;
-}
-
-// Ekipler Arasında Eşleşme Yap Fonksiyonu
-function matchTeams(teams) {
-  const matches = [];
-  const alreadyMatched = new Set();
-
-  teams.forEach((team) => {
-    const teamMembers = team.members;
-
-    for (let i = 0; i < teamMembers.length; i++) {
-      if (alreadyMatched.has(teamMembers[i]._id)) {
-        continue;
-      }
-
-      let matchFound = false;
-      let attemptCount = 0;
-
-      while (!matchFound && attemptCount < teamMembers.length) {
-        const randomIndex = Math.floor(Math.random() * teamMembers.length);
-        const user1 = teamMembers[i];
-        const user2 = teamMembers[randomIndex];
-
-        if (
-          user1 !== user2 &&
-          !alreadyMatched.has(user1._id) &&
-          !alreadyMatched.has(user2._id)
-        ) {
-          matches.push({
-            user1: { _id: user1._id, name: user1.name },
-            user2: { _id: user2._id, name: user2.name },
-          });
-          alreadyMatched.add(user1._id);
-          alreadyMatched.add(user2._id);
-          matchFound = true;
-        }
-
-        attemptCount++;
-      }
-    }
-  });
-
-  return matches;
-}
-// Kullanıcıları rastgele sıralayan fonksiyon
-function shuffleArray(array) {
-  for (let i = array.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    [array[i], array[j]] = [array[j], array[i]];
-  }
-  return array;
-}
-
-// Kullanıcıları eşleştiren fonksiyon
-function matchTeams(users) {
-  const teams = {};
-
-  // Kullanıcıları ekiplerine göre grupla
-  users.forEach((user) => {
-    const teamName = user.team; // Ekip ismini al, örneğin user.team = "aloha"
-
-    if (!teams[teamName]) {
-      teams[teamName] = [];
-    }
-
-    teams[teamName].push(user);
-  });
-
-  const matches = [];
-  const alreadyMatched = new Set();
-
-  // Her ekip içinde çekiliş yap
-  Object.keys(teams).forEach((teamName) => {
-    const teamMembers = teams[teamName];
-
-    for (let i = 0; i < teamMembers.length; i++) {
-      if (alreadyMatched.has(teamMembers[i]._id)) {
-        continue;
-      }
-
-      let matchFound = false;
-      let attemptCount = 0; // Deneme sayısı
-
-      while (!matchFound && attemptCount < teamMembers.length) {
-        const randomIndex = Math.floor(Math.random() * teamMembers.length);
-        const user1 = teamMembers[i];
-        const user2 = teamMembers[randomIndex];
-
-        if (
-          user1 !== user2 &&
-          !alreadyMatched.has(user1._id) &&
-          !alreadyMatched.has(user2._id)
-        ) {
-          matches.push({
-            user1: { _id: user1._id, name: user1.name },
-            user2: { _id: user2._id, name: user2.name },
-          });
-          alreadyMatched.add(user1._id);
-          alreadyMatched.add(user2._id);
-          matchFound = true;
-        }
-
-        attemptCount++;
-      }
-    }
-  });
-
-  return matches;
-}
-
-// Eşleştirmeleri veritabanına kaydeden fonksiyon
-async function saveMatchesToDatabase(matches) {
-  Match.createCollection();
-
-  for (const match of matches) {
-    const { user1, user2 } = match;
-
-    const matchRecord = new Match({
-      user1: user1._id,
-      user2: user2._id,
-    });
-
-    await matchRecord.save();
-  }
 }
 
 router.get("/get-matched-user/:userName", async (req, res) => {
